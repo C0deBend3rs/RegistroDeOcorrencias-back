ce6504c07d3bd1e277472ff5fdd41f4ebd3fecbf
luis <luis_oliveira@usp.br>
2021-07-28 22:42:10 -0300

    configurando middleware no route

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,14 @@ const ReportController = require('./controllers/ReportController');
 
 const routes = express.Router();
 
+routes.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 routes.post('/users/register', UsuarioController.store);   
 routes.post('/users/login', UsuarioController.login);  
