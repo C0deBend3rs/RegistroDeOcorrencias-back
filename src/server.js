5ca86696dfba6c2c59f869e4b4cf5449907b68de
luis <luis_oliveira@usp.br>
2021-07-28 22:15:43 -0300

    alterações nas conexões com o back

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const routes = require('./routes');
 
 require('./database');
+var cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 
